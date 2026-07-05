75f6d22ccfe37b7f2edc800b1157ba07c10aedf8
jsaayman-dotcom <joshsaayman14@gmail.com>
2026-07-05 16:44:44 +0200

    Create teams.js

---
+++ teams.js
@@ -0,0 +1,603 @@
+const teams = {
+
+  bulls: {
+    name: "Blue Bulls",
+
+    staff: {
+      headCoach: "Dillon Smith",
+      assistantCoach: "Chergin Fillies",
+      conditioningCoach: "De Wet Terblanche",
+      teamManager: "Carlos Augusto",
+      physio: "Thalia Beukes"
+    },
+
+    players: [
+      "Francois de Beer",
+      "Regan Blignaut",
+      "Reinhard Duvenhage",
+      "Phillip Mans",
+      "Reuben Vlok",
+      "Kwetsima Dlamini",
+      "Duran Pieterse",
+      "Janno Geyser",
+      "Tilon Baron",
+      "Caleb Africa",
+      "Brian Jacobs",
+      "David van Jaarsveld",
+      "Meyer Pretorius",
+      "Neo Ledwaba",
+      "Alex Griesel",
+      "Stiaan Theron",
+      "Esihle Mhlongo",
+      "Morne Bisset",
+      "Onyeari Didintle",
+      "Tshepang Rawida",
+      "Michael Bhuda",
+      "Kealan Cloete",
+      "Bradley Jacobs"
+    ]
+  },
+
+  limpopo: {
+    name: "Limpopo Blue Bulls",
+
+    staff: {
+      headCoach: "Kobus Delport",
+      assistantCoach: "Francois van Staden",
+      conditioningCoach: "Johan Nel",
+      teamManager: "Lloyd Makhubela"
+    },
+
+    players: [
+      "Cedric Nkuna",
+      "Rendani Mulaudzi",
+      "Neo Ramafalo",
+      "Mpho Ramashala",
+      "Kabelo Seakamela",
+      "Mpho Makhado",
+      "Katlego Mashau",
+      "Benedict Ngobeni",
+      "Thabang Mphaphuli",
+      "Mpho Raphala",
+      "Karabo Mphahlele",
+      "Aubrey Maluleke",
+      "Lesiba Mathebula",
+      "Rhulani Baloyi",
+      "Tshiamo Mphahlele",
+      "Blessing Mabunda",
+      "Khutso Malatji",
+      "Mpho Mashau",
+      "Thabang Baloyi",
+      "Mpho Mulaudzi",
+      "Katlego Makhubela",
+      "Rendani Munyai",
+      "Karabo Baloyi"
+    ]
+  },
+
+  boland: {
+    name: "Boland",
+
+    staff: {
+      headCoach: "Pieter Burger",
+      backlineCoach: "Nazeem van Sitters",
+      forwardCoach: "Heino Warnich",
+      teamManager: "Hein Pieterse"
+    },
+
+    players: [
+      "Ewan Alberts",
+      "Mattias Nell",
+      "Ernst Coetzee",
+      "Amayo Egbujor",
+      "Eugene Walker",
+      "Karl-Phillip Skors",
+      "Brendon Smith",
+      "Ryk van Zyl",
+      "Nicolai Diedericks",
+      "Ruan Nortje",
+      "Tristin Syster",
+      "Edson Fredericks",
+      "Juwaydan van Niekerk",
+      "Eldrin de Koker",
+      "Sebastien Bredell",
+      "Gerhald Laker",
+      "Leighcon du Plessis",
+      "Zinric-Dean Josephus",
+      "Hanre Liebenberg",
+      "Arthur Kana Tiotsia",
+      "Teagon Roberts",
+      "Jayden Booysen",
+      "Harold de Silva"
+    ]
+  },
+
+  border: {
+    name: "Border CD",
+
+    staff: {
+      headCoach: "Andisa Nathi Dayimani",
+      assistantCoach: "Mncedisi Mente",
+      conditioningCoach: "Teboho Mevana",
+      teamCoordinator: "Wanga Ntaba",
+      teamManager: "Luthando Xuza",
+      administrator: "Akhona Thafeni"
+    },
+
+    players: [
+      "Lungile Njikelana",
+      "Liyabona Khulu",
+      "Endinako Mzaza",
+      "Aryah Quao",
+      "Yolisa Vellem",
+      "Likho Ntleki",
+      "Lithemba Mnukwa",
+      "Liyakhanya Kodwa",
+      "Chulumanco Mayaba",
+      "Yanga Litsethu",
+      "Unako Xozwa",
+      "Akhaukhanyo Tshaka",
+      "Lisakhanya Majola",
+      "Siyamthanda Ngubane",
+      "Lilitha Goqwana",
+      "Musa Siyangaphi",
+      "Ayamkela Zwane",
+      "Athayanda Paul",
+      "Lulutho Speelman",
+      "Uviwe Madikwa",
+      "Lelethu Boyce",
+      "Gwiba Fuzile",
+      "Kwatsho Hallom"
+    ]
+  },
+    ep: {
+    name: "Eastern Province",
+
+    staff: {
+      headCoach: "Karlo Aspeling",
+      conditioningCoach: "Michael Turner",
+      assistantCoach1: "Ashwell Stride",
+      assistantCoach2: "Wylie Human",
+      teamManager: "Tyler Jade Botha"
+    },
+
+    players: [
+      "Marcus September",
+      "Luciano Solani",
+      "Lucas Wheeler",
+      "Schalk van der Merwe",
+      "Sinqobile Siyengo",
+      "David Matyani",
+      "Duncan Hudek",
+      "Zola Yeye",
+      "Kadin Kretzmann",
+      "Cole Hurter",
+      "Qhawe Madikizela",
+      "Ibenathi Kondile",
+      "Uchenna Oguekwe",
+      "Michael Nyandoro",
+      "Raqiel Jaftha",
+      "Charl du Toit",
+      "Luyolo Mpalala",
+      "Dewald Niemand",
+      "Louis van Rooyen",
+      "Ndinani Tyuka",
+      "Logan van Vuuren",
+      "Asakhe Ranuga",
+      "Matthew Goliath"
+    ]
+  },
+
+  freestate: {
+    name: "Free State",
+
+    staff: {},
+
+    players: [
+      "Shaun Rhule",
+      "WJ Dippenaar",
+      "Nicus De Jager",
+      "Neill Verster",
+      "Jannes Krause",
+      "Arno De Bruin",
+      "Hendre Maritz",
+      "Keaton Pottas",
+      "Ofentse Theko",
+      "Christoff Crous",
+      "Kia Babu",
+      "Madiba Mtanzima",
+      "Lino Mtwebana",
+      "Matthew Valentine",
+      "Ayden Cloete",
+      "Motlatsi Ramahlape",
+      "Diado Jacobs",
+      "Chris Nieuwoudt",
+      "Dian Visagie",
+      "Juan Swart",
+      "Reece Sekila",
+      "Eugene Jansen",
+      "Ze Mathese"
+    ]
+  },
+
+  griffons: {
+    name: "Griffons",
+
+    staff: {
+      headCoach: "Swys Stemmet",
+      assistantCoach: "Norman Nelson",
+      teamManager: "Kevin De Witt"
+    },
+
+    players: [
+      "JP du Toit",
+      "Jughon Juries",
+      "Unathi Mbambo",
+      "Thys Parsons",
+      "Johannes Willem Christiaan van Niekerk",
+      "Rashan Claassen",
+      "Hlombe Mamani",
+      "Caleb Butler",
+      "Theuns van Rensburg",
+      "Chriswin Delport",
+      "Shovee de Villiers",
+      "Robert van der Vyver",
+      "Alvinio Mintoor",
+      "Gimo Naftal",
+      "Jayden-Daniel Bienas",
+      "Marnus van den Berg",
+      "Barend Britz",
+      "Bokamoso Moji",
+      "Adriaan Nel",
+      "Alexander Matlou",
+      "Craydon Bibbey",
+      "Henno Kruger",
+      "Jaydrian Mietas",
+      "Eduard du Toit",
+      "Lance McDonald"
+    ]
+  },
+
+  griquas: {
+    name: "Griquas",
+
+    staff: {
+      headCoach: "Lance van Wyk",
+      assistantCoach: "Ewald van der Merwe",
+      teamManager: "Jean Julies",
+      physio: "Chris-Mari Lombaard",
+      provinceRepresentative: "J Ell Slabbert"
+    },
+
+    players: []
+  },
+    lions: {
+    name: "Lions",
+
+    staff: {
+      headCoach: "Mashudu Raphunga",
+      assistantCoach: "Lwazi Zangqa",
+      conditioningCoach: "Thulani Nzima",
+      teamManager: "Andrew Barnes"
+    },
+
+    players: [
+      "Joshua Hammann",
+      "Mahlatse Memeza",
+      "Jordan Venter",
+      "Matthew van Deventer",
+      "MC van Tonder",
+      "Llewelyn Vermaak",
+      "Naledi Phiri",
+      "Ryno van der Westhuizen",
+      "Lethabo Malahlela",
+      "Cameron Kourie",
+      "Joshua Wilkinson",
+      "Sam Smith",
+      "Juandre Ehlers",
+      "Luyanda Mbadamana",
+      "Loghan Langeveldt",
+      "Joshua Snyman",
+      "Tswaledi Phasha",
+      "Kwazi Nyoka",
+      "Hanco van Geelen",
+      "Allan Patu",
+      "Luxon Zwane",
+      "Luthando Marangxa",
+      "Asa Mgwatyu"
+    ]
+  },
+
+  leopards: {
+    name: "Leopards",
+
+    staff: {
+      headCoach: "Stephan du Toit",
+      assistantCoach: "Johan Labuschagne",
+      conditioningCoach: "Jaco Venter",
+      teamManager: "Pieter Kruger"
+    },
+
+    players: [
+      "Reuben Coetzee",
+      "Aiden Williams",
+      "Ruan Smit",
+      "Kagiso Mofokeng",
+      "Wian van der Merwe",
+      "Ruan van Zyl",
+      "Bennie Pretorius",
+      "Ryno Smit",
+      "Ethan de Beer",
+      "Jean Smith",
+      "Dian van der Walt",
+      "Luan Steyn",
+      "Tian Venter",
+      "Rian Visser",
+      "Liam Fourie",
+      "Janco van Wyk",
+      "Francois Erasmus",
+      "Marnus van Rooyen",
+      "Kobus Kruger",
+      "Johan Nel",
+      "CJ du Plessis",
+      "Ruan van Niekerk",
+      "Anrich Smit"
+    ]
+  },
+
+  lsen: {
+    name: "SA LSEN",
+
+    staff: {
+      headCoach: "Tinus van Rooyen",
+      assistantCoach: "Morne Human",
+      conditioningCoach: "Shaun Erasmus",
+      teamManager: "Riaan Oosthuizen"
+    },
+
+    players: [
+      "Player 1",
+      "Player 2",
+      "Player 3",
+      "Player 4",
+      "Player 5",
+      "Player 6",
+      "Player 7",
+      "Player 8",
+      "Player 9",
+      "Player 10",
+      "Player 11",
+      "Player 12",
+      "Player 13",
+      "Player 14",
+      "Player 15",
+      "Player 16",
+      "Player 17",
+      "Player 18",
+      "Player 19",
+      "Player 20",
+      "Player 21",
+      "Player 22",
+      "Player 23"
+    ]
+  },
+
+  pumas: {
+    name: "Pumas",
+
+    staff: {
+      headCoach: "Christiaan Gouws",
+      forwardsCoach: "Jaun Pieterse",
+      backlineCoach: "Gunther Swart",
+      defenceCoach: "Mitchelson Mkhwanazi",
+      conditioningCoach: "Zain Christians",
+      teamManager: "Kwanele Nkambule"
+    },
+
+    players: [
+      "Christiaan Viljoen",
+      "Jan-Ulrich Kinghorn",
+      "Ziyanda Olwethu Xaba",
+      "Christiaan Jaques de Klerk",
+      "Christiaan Theodore Botha",
+      "Gebrand Davel",
+      "Siyabonga Mokgoatjana",
+      "Johan Raulstone",
+      "Enio Manhica",
+      "Gaston Brown",
+      "Kgotso Mathale Peta",
+      "Janre Oosthuizen",
+      "Katlego Baloyi",
+      "Ndumiso Bandile Dlamini",
+      "Kamegelo Mohlala",
+      "Dillan Botha",
+      "WP Human",
+      "Seabi Ramodike",
+      "Nkazimulo Sinakhokonke Masondo",
+      "Aldo van Jaarsveld",
+      "Mongezi Dlamini",
+      "Carlo van Staden",
+      "Zain Hughes"
+    ]
+  },
+    sharks: {
+    name: "Sharks",
+
+    staff: {
+      headCoach: "Kelvin Edler",
+      assistantCoach1: "Burger Nel",
+      assistantCoach2: "Lyle Matthysen",
+      teamManager: "Agreement Sikhosana"
+    },
+
+    players: [
+      "Aidan Du Plooy",
+      "Rorke Stirk",
+      "Tunga Griffiths",
+      "Bulelani Cebani",
+      "Lwango Ntantala",
+      "Kevin Von Volentsee",
+      "Nala Shabangu",
+      "Aobakwe Moeng",
+      "Richaard Kriel",
+      "Tanwill Onkers",
+      "Lisa Sijadu",
+      "Elgino Oerson",
+      "Sterlin Padi",
+      "Fabiano Fierro",
+      "Rosco Williams",
+      "Matthew Wilson",
+      "Awande Mthethwa",
+      "Phokuhle Hlatshwayo",
+      "Sean Jansen",
+      "Ezra Karolisin",
+      "Ben Hughes",
+      "Sondelani Sheleni",
+      "Ruan Mulder"
+    ]
+  },
+
+  swd: {
+    name: "SWD",
+
+    staff: {},
+
+    players: [
+      "Zivan Grobbelaar",
+      "AJ Botha",
+      "Logan Jacobs",
+      "Ashley Abrahams",
+      "Liam Van Den Berg",
+      "Kane-Daniel Du Plessis",
+      "Dwayne Matthews",
+      "Aldo Gouws",
+      "Micheal Van Eeden",
+      "Mark-Hein Fourie",
+      "Ray-Jordan Schoeman",
+      "Jean Capes",
+      "Gerrit Janse van Rensburg",
+      "Quewin Pieterse",
+      "Tinashe Mazeka",
+      "Jaydee Jansen",
+      "Charles Terblanche",
+      "Avenathi Sambokwe",
+      "Franco Pienaar",
+      "Unathi Mgwexa",
+      "Verno Seconds",
+      "Luan Mortlock",
+      "Markwin Kortje"
+    ]
+  },
+    valke: {
+    name: "Valke",
+
+    staff: {
+      headCoach: "Pierre Lombard",
+      assistantCoach: "Morne Erasmus",
+      conditioningCoach: "Dean MacDonald",
+      teamManager: "Katlego Tsagane"
+    },
+
+    players: [
+      "Reuben Bothma",
+      "Dihan Keeve",
+      "Bokang Mofokeng",
+      "Matthew Robinson",
+      "Julian Breetzke",
+      "Harry Ndimande",
+      "Luca du Plooy",
+      "Rudolph du Preez",
+      "Olebogeng Rabotapi",
+      "Koketso Maphopha",
+      "Mbuso Hlophe",
+      "Vaughn Steynvaart",
+      "Conor Louw",
+      "Chester Maccamel",
+      "Ruan Opperman",
+      "Calvin Mabaso",
+      "Lesego Nkoana",
+      "Taden Bischoff",
+      "Andile Khoza",
+      "Aljay Olivier",
+      "Shandre Matroos",
+      "Kgosi Matsena",
+      "Innocent Vilakazi"
+    ]
+  },
+
+  wp: {
+    name: "Western Province",
+
+    staff: {
+      headCoach: "Curtis Beukes",
+      assistantCoach1: "Andras Molnar",
+      assistantCoach2: "Kofi Appiah",
+      strengthCoach: "Delano Rasmus",
+      physio: "Jurgens Borman",
+      executive: "Kevin Links",
+      videoAnalyst: "Uzair Saban",
+      teamManager: "Liso Langman"
+    },
+
+    players: [
+      "Ruben de Jager",
+      "Lehan Barnard",
+      "Sebastiaan Bekker",
+      "RJ Cunningham",
+      "Ruben Winter",
+      "Alvin Machinja",
+      "Dirk Hugo",
+      "Kirwan November",
+      "Daniel Arendse",
+      "Louis Koen",
+      "Aleit Stander",
+      "Louwrens Rademeyer",
+      "Kelebogile Somhlahlo Nkwana",
+      "Shelvon Mabooi",
+      "Ethon Williams",
+      "JP Norton",
+      "Boitumelo Mabohlo",
+      "Qiraan Hermans",
+      "Bernhardt Schulze",
+      "Nico Lourens",
+      "Tristan Armitage",
+      "Caleb Clark",
+      "Erchel Hanga"
+    ]
+  },
+
+  zimbabwe: {
+    name: "Zimbabwe",
+
+    staff: {},
+
+    players: [
+      "Emmanuel Tagirira",
+      "Sean Takaindisa",
+      "Emmanuel Utete",
+      "Jayden-Charles Hungwe",
+      "Nyasha Mutanga",
+      "James Manning",
+      "Bryce Honiball",
+      "Tapiwanashe Majuru",
+      "Marcus Eckard",
+      "Victor Watama",
+      "Lee Chigumba",
+      "Zivai Makunike",
+      "Russell Musekiwa",
+      "Ethan Zhou",
+      "Rukudzo Mayida",
+      "Alex Mubata",
+      "Cole Tamangani",
+      "Gavin Gorova",
+      "Adrian Manyalo",
+      "Jude Rudland",
+      "Divine Hamilton",
+      "Calvin Mudimba",
+      "Marshall Hamandishe",
+      "Tristan Van der Merwe",
+      "Jacob Viki"
+    ]
+  }
+
+};
